853295e79f8cf36f5dd7ca18b3c4861cfb8a8870
Greomory <drakshadow50@gmail.com>
2024-08-02 11:35:16 +0200

    Add Pong game files including paddles.py

--- Udemy/PongGame/main.py
+++ Udemy/PongGame/main.py
@@ -2,18 +2,38 @@ from turtle import Turtle, Screen
 from paddles import Paddle
 
 screen = Screen()
-middle_line = Turtle()
-
+line = Turtle()
 paddle_one = Paddle()
 
 screen.screensize(800, 600)
 screen.bgcolor("black")
-middle_line.goto(0, 350)
-middle_line.color("white")
-middle_line.speed("fastest")
-middle_line.right(90)
-middle_line.width(5)
-middle_line.forward(700)
+screen.title("Pong Game")
+
+def create_dotted_line():
+    line = Turtle()
+    line.color("white")
+    line.width(2)
+    line.hideturtle()
+    line.speed("fastest")
+    line.penup()
+    line.goto(0, -300)
+    line.setheading(90)
+    
+    for _ in range(31):
+        line.pendown()
+        line.forward(10)
+        line.penup()
+        line.forward(10)
+    
+    
+
+create_dotted_line()
+
+
+
+screen.onkeypress(paddle_one.move_up, "Up")
+screen.onkeypress(paddle_one.move_down, "Down")
+screen.listen()
 
 
 
--- Udemy/PongGame/paddles.py
+++ Udemy/PongGame/paddles.py
@@ -5,12 +5,21 @@ class Paddle(Turtle):
 
     def __init__(self):
         super().__init__()
+        self.penup()
         self.shape("square")
         self.color("white")
         self.shapesize(stretch_wid=0.5,stretch_len=2)
         self.settiltangle(90)
-        self.xcor()
+        self.setposition(-400,0)
+        
 
 
-    def f(self):
-        self.left(10)
+
+    def move_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    
+    def move_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
