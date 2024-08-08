4433d2db0f987c4d47f5119ce09d24667507f73a
Greomory <drakshadow50@gmail.com>
2024-08-08 10:56:03 +0200

    added the highscore to data

--- Udemy/File_management/file.py
+++ Udemy/File_management/file.py
@@ -1,5 +1,8 @@
 
-score = open("score.txt")
-contents = score.read()
+""" with open("score.txt") as file:
+    contents = file.read()
+    print(contents)
+    file.close() """ 
 
-print(contents)
+with open("new_score.txt", mode="a") as file:
+    file.write("\n New text.")
