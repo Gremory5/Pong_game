9cec4796ffd4cf86dc0acf9e1cd16bc9fe50c279
Greomory <drakshadow50@gmail.com>
2024-10-01 15:24:41 +0200

    new file:   Udemy/day25/squirrel_data.csv

--- Udemy/day25/main.py
+++ Udemy/day25/main.py
@@ -7,12 +7,11 @@ with open("weather_data.csv") as data_file:
         if row[1] != "temp":
             temperatures.append(int(row[1]))        
     print(temperatures) """
-
+""" 
 import pandas as pd
-
 data = pd.read_csv("weather_data.csv")
-""" print(type(data)) """
-""" #  print(data["temp"])
+print(type(data)) 
+#  print(data["temp"])
 
 data_dict = data.to_dict()
 print(data_dict)
@@ -35,7 +34,7 @@ monday = data[data.day == "Monday"]
 monday_far = monday.temp * 9/5 + 32
 
 print(monday_far)
-"""
+
 
 #Create a dataframe from scratch
 
@@ -45,4 +44,17 @@ data_dict = {
 }
 data = pd.DataFrame(data_dict)
 data.to_csv("new_data.csv")
+"""
+import pandas as pd
 
+data = pd.read_csv("squirrel_data.csv")
+grey_squirrels_count = len(data[data["Primary Fur Color"] == "Gray"])
+red_squirrels_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrels_count = len(data[data["Primary Fur Color"] == "Black"])
+
+squirrelounts = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [grey_squirrels_count, red_squirrels_count, black_squirrels_count]
+}
+df = pd.DataFrame(squirrelounts)
+df.to_csv("squirrel_count.csv")
